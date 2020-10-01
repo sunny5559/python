7da6cfd0b98b95023bfbe70331401dd8c98e9429
Manjeet Haryani <64313754+manjeet-haryani@users.noreply.github.com>
2020-10-02 01:21:20 +0530

    Update datatypes.py

--- basics/datatypes.py
+++ basics/datatypes.py
@@ -1,6 +1,6 @@
 a = 10
 f = 10.23
-s = "you are the creator of your destiny"
+s = "you are the creator of your Destiny"
 print ("five types\n None type an object that does not contain any value")
 print (" numeric- int,float and complex")
 print ("sequences - str, bytes, bytearray, list and tuple, range")
